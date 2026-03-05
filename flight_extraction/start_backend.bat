1ccb0d8c308ab16d77a0c3c77802e46724630317
Sujal <sujal.dhamne@operisoft.com>
2026-03-05 17:04:23 +0530

    Add back start_backend.bat

---
+++ flight_extraction/start_backend.bat
@@ -0,0 +1,8 @@
+@echo off
+echo Starting Travel Document Extractor Backend...
+echo.
+echo Make sure you have:
+echo - Python dependencies installed (pip install -r ../requirements.txt)
+echo - AWS credentials configured
+echo.
+python api.py
